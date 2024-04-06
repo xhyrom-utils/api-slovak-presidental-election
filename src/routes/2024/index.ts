b004b43de635f698ea2aa30d8e776b776d00a96d
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-04-06 21:00:35 +0200

    refactor: change default /2024

--- src/routes/2024/index.ts
+++ src/routes/2024/index.ts
@@ -4,18 +4,8 @@ import second from "./second";
 
 const router = new Hono();
 
-router.route("/", first);
+router.route("/", second);
 router.route("/first", first);
-
-router.use("/second/*", async (c, next) => {
-  // 6th april 2024
-  if (Date.now() < 1712354400000) {
-    c.status(404);
-    return c.json({ error: "No data yet" });
-  }
-
-  await next();
-});
 router.route("/second", second);
 
 export default router;
